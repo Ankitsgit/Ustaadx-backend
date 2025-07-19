21d8a5e20f3ceaeb01164e1eb6fe5322ce17d095
Ankitsgit <ankit77us@gmail.com>
2025-07-20 02:51:55 +0530

    request booking update

--- src/models/Booking.js
+++ src/models/Booking.js
@@ -6,10 +6,10 @@ const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema(
   {
-  fromUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  fromUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User',unique: true  },
   toUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  skill: { type: String },
-  timeslot: { type: String },
+  skill: { type: String , required: true},
+  timeslot: { type: String, required: true },
   message: { type: String },
   status: {
     type: String,
